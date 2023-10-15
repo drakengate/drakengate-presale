201f49431ecdcba6f259c5b69d6b10d591af3396
developer <samsonjohnn@yandex.com>
2023-10-15 22:14:22 +0300

    title favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Drakengate Presale</title>
       <body>
         <Main />
         <NextScript />
